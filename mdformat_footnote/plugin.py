135970477228daa43b118be0b561769820771385
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2021-04-13 00:36:40 +0300

    Sync with mdformat 0.7.x

--- mdformat_footnote/plugin.py
+++ mdformat_footnote/plugin.py
@@ -1,8 +1,8 @@
-from typing import Mapping, MutableMapping
+from typing import Mapping
 
 from markdown_it import MarkdownIt
-from mdformat.renderer import RenderTreeNode
-from mdformat.renderer.typing import RendererFunc
+from mdformat.renderer import RenderContext, RenderTreeNode
+from mdformat.renderer.typing import Render
 from mdit_py_plugins.footnote import footnote_plugin
 
 
@@ -14,45 +14,28 @@ def update_mdit(mdit: MarkdownIt) -> None:
     mdit.disable("footnote_inline")
 
 
-def _footnote_ref_renderer(
-    node: RenderTreeNode,
-    renderer_funcs: Mapping[str, RendererFunc],
-    options: Mapping,
-    env: MutableMapping,
-) -> str:
+def _footnote_ref_renderer(node: RenderTreeNode, context: RenderContext) -> str:
     return f"[^{node.meta['label']}]"
 
 
-def _footnote_renderer(
-    node: RenderTreeNode,
-    renderer_funcs: Mapping[str, RendererFunc],
-    options: Mapping,
-    env: MutableMapping,
-) -> str:
+def _footnote_renderer(node: RenderTreeNode, context: RenderContext) -> str:
     text = f"[^{node.meta['label']}]: "
     child_iterator = iter(node.children)
     first_child = next(child_iterator)
     if first_child.type == "footnote_anchor":
         return text
     else:
-        text += first_child.render(renderer_funcs, options, env)
+        text += first_child.render(context)
     for child in child_iterator:
-        text += "\n\n    " + child.render(renderer_funcs, options, env)
+        text += "\n\n    " + child.render(context)
     return text
 
 
-def _render_children(
-    node: RenderTreeNode,
-    renderer_funcs: Mapping[str, RendererFunc],
-    options: Mapping,
-    env: MutableMapping,
-) -> str:
-    return "\n\n".join(
-        child.render(renderer_funcs, options, env) for child in node.children
-    )
+def _render_children(node: RenderTreeNode, context: RenderContext) -> str:
+    return "\n\n".join(child.render(context) for child in node.children)
 
 
-RENDERER_FUNCS: Mapping[str, RendererFunc] = {
+RENDERERS: Mapping[str, Render] = {
     "footnote": _footnote_renderer,
     "footnote_ref": _footnote_ref_renderer,
     "footnote_block": _render_children,
