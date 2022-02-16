87c4f3b90ae012c59af273450b75fa5ad037b5ff
Taneli Hukkinen <3275109+hukkin@users.noreply.github.com>
2022-02-16 11:06:04 +0200

    🔧 MAINTAIN: Drop support for EOL Python 3.6 (#4)

--- mdformat_footnote/plugin.py
+++ mdformat_footnote/plugin.py
@@ -1,5 +1,7 @@
+from __future__ import annotations
+
+from collections.abc import Mapping
 import textwrap
-from typing import Mapping
 
 from markdown_it import MarkdownIt
 from mdformat.renderer import RenderContext, RenderTreeNode
