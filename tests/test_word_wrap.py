d3cd3a95beec57b352289c52de3fb035396bcbfe
Kyle King <KyleKing@users.noreply.github.com>
2026-01-27 08:59:40 -0500

    🐛 FIX: properly wrap footnotes (#24)

--- tests/test_word_wrap.py
+++ tests/test_word_wrap.py
@@ -1,25 +1,26 @@
+from pathlib import Path
+import re
+
+from markdown_it.utils import read_fixture_file
 import mdformat
+import pytest
+
+FIXTURE_PATH = Path(__file__).parent / "fixtures_wrap.md"
+fixtures = read_fixture_file(FIXTURE_PATH)
 
 
-def test_word_wrap():
-    input_text = """\
-[^a]
+def _extract_wrap_length(title):
+    if match := re.search(r"wrap at (\d+)", title):
+        return int(match.group(1))
+    return 40
 
-[^a]: Ooh no, the first line of this first paragraph is still wrapped too wide
-    unfortunately. Should fix this.
 
-    But this second paragraph is wrapped exactly as expected. Woohooo, awesome!
-"""
-    expected_output = """\
-[^a]
-
-[^a]: Ooh no, the first line of this first
-    paragraph is still wrapped too wide
-    unfortunately. Should fix this.
-
-    But this second paragraph is wrapped
-    exactly as expected. Woohooo,
-    awesome!
-"""
-    output = mdformat.text(input_text, options={"wrap": 40}, extensions={"footnote"})
-    assert output == expected_output
+@pytest.mark.parametrize(
+    "line,title,text,expected",
+    fixtures,
+    ids=[f[1] for f in fixtures],
+)
+def test_word_wrap(line, title, text, expected):
+    wrap_length = _extract_wrap_length(title)
+    output = mdformat.text(text, options={"wrap": wrap_length}, extensions={"footnote"})
+    assert output.rstrip() == expected.rstrip(), output
