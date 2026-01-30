5e3f44bf6614489600c080138c4d7bd69fcfb6d6
Kyle King <KyleKing@users.noreply.github.com>
2026-01-30 09:11:24 -0500

    🚀 RELEASE: 0.1.3 (#25)

--- mdformat_footnote/__init__.py
+++ mdformat_footnote/__init__.py
@@ -1,6 +1,6 @@
 """An mdformat plugin for parsing/validating footnotes"""
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __plugin_name__ = "footnote"
 
 from .plugin import RENDERERS, add_cli_argument_group, update_mdit  # noqa: F401
